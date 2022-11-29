add7c7b787da06960179c76cd0f89c89e6f13544
Anas Juwaidi <anas.didi95@gmail.com>
2022-11-29 21:00:04 +0800

    ADD: Play audio upon click button

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,11 +1,21 @@
-import React from "react"
-import logo from "./logo.svg"
+import { useRef } from "react"
 import "./App.css"
+import sound from "./assets/sound_pop.ogg"
+import logo from "./logo.svg"
 
 function App() {
+  const audioRef = useRef<HTMLAudioElement>(null)
+
+  const handlePlaySound = async () => {
+    const audio = new Audio(sound)
+    await audio.play()
+  }
+
   return (
     <div className="App">
+      <audio id="clearsound" ref={audioRef} src={sound} preload="auto" />
       <header className="App-header">
+        <button onClick={handlePlaySound}>Play sound</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
