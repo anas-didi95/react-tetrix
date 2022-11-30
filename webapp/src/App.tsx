a184bf007dbef5ceca2eb89e401393e8a96e5981
Anas Juwaidi <anas.didi95@gmail.com>
2022-11-30 20:14:41 +0800

    ADD: Add canvas

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,33 +1,10 @@
-import { useRef } from "react"
 import "./App.css"
-import sound from "./assets/sound_pop.ogg"
-import logo from "./logo.svg"
 
 function App() {
-  const audioRef = useRef<HTMLAudioElement>(null)
-
-  const handlePlaySound = async () => {
-    const audio = new Audio(sound)
-    await audio.play()
-  }
-
   return (
     <div className="App">
-      <audio id="clearsound" ref={audioRef} src={sound} preload="auto" />
-      <header className="App-header">
-        <button onClick={handlePlaySound}>Play sound</button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <canvas width="300" height="600" />
+      <button id="playbutton" onClick={() => console.log("PLAY")}>Play</button>
     </div>
   )
 }
