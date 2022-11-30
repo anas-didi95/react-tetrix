fa5b6a9063df11234b594a13c4dce4bd4417e4c8
Anas Juwaidi <anas.didi95@gmail.com>
2022-11-30 20:23:30 +0800

    ADD: Disable button Play upon start game

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react"
 import "./App.css"
 
-function App() {
+const App: React.FC<{}> = () => {
+  const [onPlay, setOnPlay] = useState(false)
+
+  const handlePlay = () => {
+    setOnPlay(true)
+  }
+
   return (
     <div className="App">
       <canvas width="300" height="600" />
-      <button id="playbutton" onClick={() => console.log("PLAY")}>Play</button>
+      <button id="playbutton" disabled={onPlay} onClick={handlePlay}>
+        Play
+      </button>
     </div>
   )
 }
